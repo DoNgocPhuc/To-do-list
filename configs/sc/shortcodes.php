8dbfc2382fb342b6d7d81394835cf5f66d9cdf36
Wiloke <contact.wilcity@gmail.com>
2021-02-25 14:28:25 +0700

    First commit
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

---
+++ configs/sc/shortcodes.php
@@ -0,0 +1,26 @@
+<?php
+return [
+	'wilcity_kc_hsblog_cats' => [
+		'name'     => 'HsBlog Categories',
+		'icon'     => 'sl-paper-plane',
+		'group'    => 'term',
+		'css_box'  => true,
+		'category' => WILCITY_SC_CATEGORY,
+		'params'   => [
+			'general' => [
+				'heading',
+				'heading_color',
+				'desc',
+				'desc_color',
+				'header_desc_text_align',
+				'hsblog_cats'
+			],
+			'styling' => [
+				[
+					'name' => 'css_custom',
+					'type' => 'css'
+				]
+			]
+		]
+	]
+];
